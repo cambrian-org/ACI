2a613fdc6c1fdf2441d7e18e0290e2d828498b56
AaronYoung5 <AaronYoung5@users.noreply.github.com>
2025-08-26 06:42:25 +0000

    deploy: 557208c68a5586bbd4d989e8be10efb37657a6e8

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev595+g919400b',
+    VERSION: '0.0.post1.dev596+g557208c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
